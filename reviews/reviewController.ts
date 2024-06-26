1ed948dc31d8903fb0ed34ea96827ff5929f0baa
kimenyu <njorogekimenyu@gmail.com>
2024-06-26 17:16:29 +0300

    adding review controllers

---
+++ reviews/reviewController.ts
@@ -0,0 +1,105 @@
+import { Request, Response } from 'express';
+import Review from '../models/reviewModel';
+import Appointment from '../models/appointmentModel';
+
+interface AuthRequest extends Request {
+  params: any;
+  body: { appointmentId: any; rating: any; comment: any; };
+  user?: any;
+}
+
+export const createReview = async (req: AuthRequest, res: Response) => {
+  try {
+    const { appointmentId, rating, comment } = req.body;
+    if (!req.user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const appointment = await Appointment.findById(appointmentId);
+    if (!appointment) {
+      return res.status(404).send({ error: 'Appointment not found' });
+    }
+
+    if (appointment.user.toString() !== req.user._id.toString()) {
+      return res.status(403).send({ error: 'Forbidden' });
+    }
+
+    const review = new Review({
+      user: req.user._id,
+      staff: appointment.staff,
+      service: appointment.services[0], // Assuming one service per appointment
+      rating,
+      comment
+    });
+
+    await review.save();
+    res.status(201).send(review);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const getReviews = async (req: Request, res: Response) => {
+  try {
+    const reviews = await Review.find({}).populate('user staff service');
+    res.send(reviews);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const getReview = async (req: Request, res: Response) => {
+  try {
+    const review = await Review.findById(req.params.id).populate('user staff service');
+    if (!review) {
+      return res.status(404).send({ error: 'Review not found' });
+    }
+    res.send(review);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const updateReview = async (req: AuthRequest, res: Response) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['rating', 'comment'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const review = await Review.findById(req.params.id);
+    if (!review) {
+      return res.status(404).send({ error: 'Review not found' });
+    }
+
+    if (review.user.toString() !== req.user._id.toString()) {
+      return res.status(403).send({ error: 'Forbidden' });
+    }
+
+    updates.forEach(update => (review[update] = req.body[update]));
+    await review.save();
+    res.send(review);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deleteReview = async (req: AuthRequest, res: Response) => {
+  try {
+    const review = await Review.findByIdAndDelete(req.params.id);
+    if (!review) {
+      return res.status(404).send({ error: 'Review not found' });
+    }
+
+    if (review.user.toString() !== req.user._id.toString()) {
+      return res.status(403).send({ error: 'Forbidden' });
+    }
+
+    res.send(review);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
