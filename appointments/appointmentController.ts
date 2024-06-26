ca67291e78e6f593a7fdad56bc45282561e63a1c
kimenyu <njorogekimenyu@gmail.com>
2024-06-26 17:13:33 +0300

    adding appointment and service controllers

---
+++ appointments/appointmentController.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from 'express';
+import Appointment from '../models/appointmentModel';
+import User from '../models/userModel';
+import Staff from '../models/staffModel';
+import Service from '../models/serviceModel';
+
+interface AuthRequest extends Request {
+  params: any;
+  body: { user: any; staff: any; services: any; date: any; };
+  user?: any;
+}
+
+export const createAppointment = async (req: AuthRequest, res: Response) => {
+  try {
+    const { user, staff, services, date } = req.body;
+    if (!req.user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const existingAppointments = await Appointment.find({
+      staff,
+      date: { $eq: new Date(date) }
+    });
+
+    if (existingAppointments.length) {
+      return res.status(400).send({ error: 'Staff is not available at the selected time' });
+    }
+
+    const appointment = new Appointment({
+      user: req.user._id,
+      staff,
+      services,
+      date,
+      status: 'booked',
+      totalPrice: await calculateTotalPrice(services)
+    });
+
+    await appointment.save();
+    res.status(201).send(appointment);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+const calculateTotalPrice = async (services: string[]) => {
+  let totalPrice = 0;
+  for (const serviceId of services) {
+    const service = await Service.findById(serviceId);
+    if (service) {
+      totalPrice += service.price;
+    }
+  }
+  return totalPrice;
+};
+
+export const getAppointments = async (req: AuthRequest, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const appointments = await Appointment.find({ user: req.user._id }).populate('staff services');
+    res.send(appointments);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const cancelAppointment = async (req: AuthRequest, res: Response) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id);
+    if (!appointment) {
+      return res.status(404).send({ error: 'Appointment not found' });
+    }
+
+    if (appointment.user.toString() !== req.user._id.toString()) {
+      return res.status(403).send({ error: 'Forbidden' });
+    }
+
+    appointment.status = 'cancelled';
+    await appointment.save();
+    res.send(appointment);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
---
+++ royalsheerservices/yoyalSheerServicesControllers.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express';
+import Service from '../models/serviceModel';
+
+export const createService = async (req: Request, res: Response) => {
+  try {
+    const service = new Service(req.body);
+    await service.save();
+    res.status(201).send(service);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const getServices = async (req: Request, res: Response) => {
+  try {
+    const services = await Service.find({});
+    res.send(services);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const getService = async (req: Request, res: Response) => {
+  try {
+    const service = await Service.findById(req.params.id);
+    if (!service) {
+      return res.status(404).send({ error: 'Service not found' });
+    }
+    res.send(service);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const updateService = async (req: Request, res: Response) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['type', 'description', 'price', 'duration', 'addOns'];
+  const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const service = await Service.findById(req.params.id);
+    if (!service) {
+      return res.status(404).send({ error: 'Service not found' });
+    }
+
+    updates.forEach(update => (service[update] = req.body[update]));
+    await service.save();
+    res.send(service);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deleteService = async (req: Request, res: Response) => {
+  try {
+    const service = await Service.findByIdAndDelete(req.params.id);
+    if (!service) {
+      return res.status(404).send({ error: 'Service not found' });
+    }
+    res.send(service);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
