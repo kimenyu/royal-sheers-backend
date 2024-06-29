f76e38034744d68f7d9d4d4b68920db11093aafb
kimenyu <njorogekimenyu@gmail.com>
2024-06-29 10:28:44 +0300

    updating the product model

--- models/productModel.ts
+++ models/productModel.ts
@@ -5,13 +5,15 @@ interface IProduct extends Document {
   description?: string;
   price: number;
   stock: number;
+  image?: string;
 }
 
 const productSchema: Schema<IProduct> = new Schema({
   name: { type: String, required: true },
   description: { type: String },
   price: { type: Number, required: true },
-  stock: { type: Number, required: true }
+  stock: { type: Number, required: true },
+  image: { type: String },
 });
 
 const Product = mongoose.model<IProduct>('Product', productSchema);
