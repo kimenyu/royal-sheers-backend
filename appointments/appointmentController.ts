b01bad9b81a7fb7205cedf5998ee3783fae7ce31
kimenyu <njorogekimenyu@gmail.com>
2024-07-07 15:16:55 +0300

    modify get appointments controller

--- appointments/appointmentController.ts
+++ appointments/appointmentController.ts
@@ -89,7 +89,7 @@ export const getAppointments = async (req: AuthRequest, res: Response) => {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    const appointments = await Appointment.find({ user: user.Id });
+    const appointments = await Appointment.find({ user: user.Id }).populate('services');
     res.send(appointments);
   } catch (error) {
     res.status(500).send(error);
