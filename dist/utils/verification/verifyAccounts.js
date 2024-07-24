511e3db918656d0bcb368b8f974cba2d936679ad
kimenyu <njorogekimenyu@gmail.com>
2024-07-24 12:50:35 +0300

    adding dist folder in gitignore

--- dist/utils/verification/verifyAccounts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = generateVerificationCode;
-function generateVerificationCode() {
-    return Math.floor(10000 + Math.random() * 90000);
-}
-//# sourceMappingURL=verifyAccounts.js.map
